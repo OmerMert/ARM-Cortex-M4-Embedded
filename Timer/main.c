af0265effbaef2c8d91e3f592d34033e64b63bc0
OmerMert <myurdakul65@gmail.com>
2022-09-27 16:39:56 +0300

    Created Timer Project

---
+++ Timer/main.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+int main(void)
+{
+    /* Configuration for Blue LED on Port F */
+    SYSCTL_RCGCGPIO_R |= (1<<5);
+    GPIO_PORTF_AFSEL_R &= ~(1<<2);
+    GPIO_PORTF_DIR_R |= (1<<2);
+    GPIO_PORTF_DEN_R |= (1<<2);
+    GPIO_PORTF_DATA_R |= (1<<2);
+
+    /* Configuration for Periodic General-Purpose Timer 0A */
+    SYSCTL_RCGCTIMER_R |= (1<<0);
+    TIMER0_CTL_R &= ~(1<<0);        // Disable Timer
+    TIMER0_CFG_R = 0x00000000;
+    TIMER0_TAMR_R |= (0x2<<0);      // Set Periodic Timer Mode
+    TIMER0_TAMR_R &= ~(1<<4);       // Set Count Down Mode
+    TIMER0_TAILR_R = 0x00F42400;    // Set Start Value 16.000.000
+    TIMER0_CTL_R |= (1<<0);         // Enable Timer
+
+    while(1)
+    {
+        if((TIMER0_RIS_R & 0x00000001) == 1)
+        {
+            TIMER0_ICR_R |= (1<<0);         //Clear TIMER0_RIS_R
+            GPIO_PORTF_DATA_R ^= (1<<2);    //Toggle Blue LED
+        }
+
+    }
+
+
+
+	return 0;
+}
